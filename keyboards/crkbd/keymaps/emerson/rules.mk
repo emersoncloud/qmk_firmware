92b55de46d56657e4d05bca107bda8609a2f5c70
Emerson Cloud <ecloud@hubspot.com>
2020-03-16 20:14:37 -0400

    add emerson map

---
+++ keyboards/crkbd/keymaps/emerson/rules.mk
@@ -0,0 +1,12 @@
+COMBO_ENABLE = yes
+TAP_DANCE_ENABLE=no
+MOUSEKEY_ENABLE = yes
+SRC +=  ./lib/glcdfont.c \
+        ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        ./lib/timelogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
