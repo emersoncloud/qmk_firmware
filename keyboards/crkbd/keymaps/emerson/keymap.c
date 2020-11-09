e184c859b66d9386dac1f58d109dde871ac5598a
Emerson Cloud <ecloud@hubspot.com>
2020-11-09 15:45:28 -0500

    change thumb button in gaming mode

--- keyboards/crkbd/keymaps/emerson/keymap.c
+++ keyboards/crkbd/keymaps/emerson/keymap.c
@@ -94,9 +94,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RALT,\
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, LOWER,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          LOWER,  KC_SPC,  KC_LCTL,    RAISE, KC_SPC, KC_ENT \
+                                          KC_LALT,  KC_SPC,  KC_LCTL,    RAISE, KC_SPC, KC_ENT \
                                       //--------------------------'  --------------------------'
   ),
 
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
     ),
 
-      
+
 
   [_LOWER] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -296,7 +296,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_off(_ADJUST);
         }
         return false;
-    case WINDOWS: 
+    case WINDOWS:
         if (record->event.pressed) {
           layer_on(_WINDOWS);
         } else {
